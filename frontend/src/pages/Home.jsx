c11ec00f1fe031f556664e352f3533230b990e28
Jennifer-Hsueh <ada.ling111@gmail.com>
2026-08-12 13:28:09 +0000

    style: move home watermark logo position

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -44,7 +44,7 @@ export default function Home() {
 <img
   src="/home-watermark-logo.png"
   alt=""
-  className="pointer-events-none absolute bottom-20 right-4 opacity-[1.00]"
+  className="pointer-events-none absolute bottom-20 right-4 opacity-[0.10]"
   style={{ width: '432px', maxWidth: 'none' }}
 />
 
